db5ba878edeb266aa5c22a3c5a25b3546a0a1a90
Sergio Hidalgo <shidalgoc@auna.pe>
2022-07-03 09:19:08 -0500

    lazy load

--- projects/sistema/src/app/app-routing.module.ts
+++ projects/sistema/src/app/app-routing.module.ts
@@ -2,7 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 
-const routes: Routes = [{ path: '', component: TestComponent }];
+const routes: Routes = [
+  { path: '', component: TestComponent },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./user/user.module').then((module) => module.UserModule),
+  },
+  {
+    path: 'driver',
+    loadChildren: () =>
+      import('./driver/driver.module').then((module) => module.DriverModule),
+  },
+  {
+    path: 'medic',
+    loadChildren: () =>
+      import('./medic/medic.module').then((module) => module.MedicModule),
+  },
+  {
+    path: 'history',
+    loadChildren: () =>
+      import('./history/history.module').then((module) => module.HistoryModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
